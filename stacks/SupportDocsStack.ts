f76f2d2ce9e860279d468f35d102e592cd470cce
Evan Young <715756+YourBassHertz@users.noreply.github.com>
2022-06-15 16:08:41 -0700

    Trying the github action workflow.

--- stacks/SupportDocsStack.ts
+++ stacks/SupportDocsStack.ts
@@ -13,7 +13,14 @@ export function SupportDocsStack({ stack }: StackContext) {
 
   // Create a Next.js site
   const site = new NextjsSite(stack, "Site", {
-    path: "frontend"
+    path: "frontend",
+    environment: {
+      // Pass the table details to our app
+      NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID ? process.env.NEXT_PUBLIC_GTM_ID : '',
+      NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ? process.env.NEXT_PUBLIC_ALGOLIA_APP_ID : '',
+      NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY ? process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY : '',
+      ALGOLIA_SEARCH_ADMIN_KEY: process.env.ALGOLIA_SEARCH_ADMIN_KEY ? process.env.ALGOLIA_SEARCH_ADMIN_KEY : '',
+    },
   });
 
   // Show the site URL in the output
